0e15baded8c9ef2dcf51373ec490e5be27eb4da6
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-10-17 13:29:40 +0300

    create contact information

---
+++ android/app/src/main/kotlin/com/example/depihivetask/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.depihivetask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ContactInformation());
+}
+class ContactInformation extends StatelessWidget {
+  const ContactInformation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
